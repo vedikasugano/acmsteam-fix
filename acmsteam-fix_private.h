339fc9f2ca2ef1e131d62f8b8b2293d2ab3f844f
vedikasugano <vedikasugano@gmail.com>
2015-08-23 13:11:43 -0300

    First upload

---
+++ acmsteam-fix_private.h
@@ -0,0 +1,23 @@
+/* THIS FILE WILL BE OVERWRITTEN BY DEV-C++ */
+/* DO NOT EDIT ! */
+
+#ifndef ACMSTEAM-FIX_PRIVATE_H
+#define ACMSTEAM-FIX_PRIVATE_H
+
+/* VERSION DEFINITIONS */
+#define VER_STRING	"0.0.1.0"
+#define VER_MAJOR	0
+#define VER_MINOR	0
+#define VER_RELEASE	1
+#define VER_BUILD	0
+#define COMPANY_NAME	""
+#define FILE_VERSION	"0.0.1.0"
+#define FILE_DESCRIPTION	"Loop fix for A:CM"
+#define INTERNAL_NAME	""
+#define LEGAL_COPYRIGHT	"Vedi Kasugano"
+#define LEGAL_TRADEMARKS	""
+#define ORIGINAL_FILENAME	""
+#define PRODUCT_NAME	""
+#define PRODUCT_VERSION	""
+
+#endif /*ACMSTEAM-FIX_PRIVATE_H*/
---
+++ main.c
@@ -0,0 +1,32 @@
+#include <windows.h>
+
+int main(int argc, char *argv[])
+{
+   int x;
+   unsigned char cmdline[32768];
+  
+   STARTUPINFO si;
+   PROCESS_INFORMATION pi;
+   ZeroMemory(&si, sizeof(si));
+   si.cb = sizeof(si);
+   si.dwFlags = STARTF_USESHOWWINDOW;
+   si.wShowWindow = SW_NORMAL;
+   ZeroMemory(&pi, sizeof(pi));
+   ZeroMemory(&cmdline, 32768);
+  
+   lstrcpy(cmdline, "_ACM.exe");
+  
+   for(x=1;x<argc;x++)
+   {
+      lstrcat(cmdline, " ");
+      lstrcat(cmdline, argv[x]);
+   }
+  
+   if(!CreateProcess(0, cmdline, 0, 0, FALSE, 0, NULL, NULL, &si, &pi))
+   		MessageBox(NULL, "Can't launch _ACM.exe", "Error", MB_ICONERROR | MB_OK);
+		
+   WaitForSingleObject(pi.hProcess, INFINITE);
+   CloseHandle(pi.hProcess);
+   CloseHandle(pi.hThread);
+   return 0;
+}
